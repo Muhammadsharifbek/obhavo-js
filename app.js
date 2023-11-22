aa622deea4b9374dcd1055cfe7a58bcc1e7b5341
Muhammadsharifbek <sharifahmadov690@gmail.com>
2023-11-22 19:20:54 +0500

    add all settings

---
+++ app.js
@@ -0,0 +1,50 @@
+const changeLocation = document.getElementById("change-location");
+const card = document.getElementById("card");
+const details = document.getElementById("details");
+const weatherIcon = document.getElementById("weather-icon");
+const overlay = document.getElementById("overlay");
+const btn = document.getElementById("btn");
+
+changeLocation.city.focus();
+
+const updateUI = (weather) => {
+  console.log(weather);
+  details.innerHTML = `
+<h5 class="mb-3">${weather.name}, ${weather.sys.country}</h5>
+<p class="mb-3">${weather.weather[0].main}</p>
+<div class="display-4 mb-3">
+  <span>${Math.round(weather.main.temp)}</span>
+  <span>&deg;C</span>
+</div>
+`;
+
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+
+  weatherIcon.src = ` https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
+};
+
+const getWeather = async (city) => {
+  const data = await getData(city);
+
+  return data;
+};
+
+const fetchWeatherAndUpdateUI = (cityName) => {
+  getWeather(cityName).then((data) => updateUI(data));
+};
+
+changeLocation.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const cityName = changeLocation.city.value.trim();
+  changeLocation.reset();
+  fetchWeatherAndUpdateUI(cityName);
+  getWeather(cityName).then((data) => updateUI(data));
+});
+
+btn.addEventListener("submit", () => {
+  const cityName = changeLocation.city.value.trim();
+  fetchWeatherAndUpdateUI(cityName);
+});
